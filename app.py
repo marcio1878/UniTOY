2330aea469e3a361c9b7c1865c7d80e0911e463f
MARCIO <162049400+marcio1878@users.noreply.github.com>
2026-05-25 21:14:24 -0300

    Removendo credenciais

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def conectar():
         return mysql.connector.connect(
             host='127.0.0.1',
             user='aluno',
-            password='',
+            password='123',
             database='projeto_vendas_unitoy'
         )
     except Exception as erro:
